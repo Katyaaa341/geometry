d3672e91674115a563ec574641b4d95387bf9272
Ekaterina Ilinykh <ilinykh_katyaa@mail.ru>
2020-06-07 21:27:10 +0700

    add foo-check of figure

---
+++ name_of_figure.c
@@ -0,0 +1,9 @@
+#include <string.h>
+
+int nameFigure(char *figure)
+{
+    if (strncmp(figure, "circle", 6) == 0) {
+        return 0;
+    }
+    return 1;
+}
---
+++ name_of_figure.h
@@ -0,0 +1,4 @@
+#ifndef GEOMETRY_NAME_OF_FIGURE_H
+#define GEOMETRY_NAME_OF_FIGURE_H
+int nameFigure(char *figure);
+#endif
