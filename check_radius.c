c9f8ed291a3e2eeacb5ae92c2fb1ede3ed9f6e36
Ekaterina Ilinykh <ilinykh_katyaa@mail.ru>
2020-06-07 21:26:18 +0700

    add foo-check of radius

---
+++ check_radius.c
@@ -0,0 +1,7 @@
+int checkRadius(int radius)
+{
+    if (radius == 0 || radius < 0) {
+        return 1;
+    }
+    return 0;
+}
---
+++ check_radius.h
@@ -0,0 +1,4 @@
+#ifndef GEOMETRY_CHECK_RADIUS_H
+#define GEOMETRY_CHECK_RADIUS_H
+int checkRadius(int radius);
+#endif
